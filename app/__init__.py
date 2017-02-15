402f01bb63ef393a0fccfe5b87d193e25672bba4
mnjaggah <maria.njaggah@gmail.com>
2017-02-15 07:28:36 +0300

    Update Configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+#from flask_sqlalchemy import SQLAlchemy
+#from config import app_config
 
 #initialize the app
 app = Flask(__name__, instance_relative_config=True)
@@ -6,5 +8,20 @@ app = Flask(__name__, instance_relative_config=True)
 #Load the views
 from app import views
 
-#Load config file
+#load config file
 app.config.from_object('config')
+
+#db = SQLAlchemy
+#Load config file
+'''def create_app(config_name):
+	app = Flask(__name__, instance_relative_config=True)
+	app.config.from_object(app_config[config_name])
+	app.config.from_pyfile('config.py')
+	db.init_app(app)
+
+	@app.route('/')
+	def hello_world():
+		return 'Hello, World!'
+
+	return app
+'''
