e8ab08bb8ca01b17fa3340be8a96c6ca4a74f6cf
mnjaggah <maria.njaggah@gmail.com>
2017-02-17 01:06:16 +0300

    Fix bugs resulting from import

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,41 +2,39 @@ from flask import flash, redirect, render_template, url_for
 from flask_login import login_required, login_user, logout_user
 
 from . import auth
-from forms import LoginForm, Registration
+from .forms import LoginForm, Registration
 from .. import db
 from ..models import User
 
-
 @auth.route('/register', methods=['GET', 'POST'])
-def sign_up():
+def register():
     form = Registration()
-	if form.validate_on_submit():
-		user = User(first_name = form.first_name.data,
-					last_name = form.last_name.data,
-					email = form.email.data,
-					username = form.username.data,
-					password = form.password.data,
-					confirm = form.confirm.data
-					)
-		db.session.add(user)
-		db.session.commit()
-		flash('Thank you for registering')
-		return redirect(url_for('auth.sign_in'))
-	return render_template("auth/register.html", form=form, title="Sign Up")
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                            username=form.username.data,
+                            first_name=form.first_name.data,
+                            last_name=form.last_name.data,
+                            password=form.password.data)
 
+    
+        db.session.add(user)
+        db.session.commit()
+        flash('You have successfully registered! You may now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form, title='Register')
 
-@auth.route('/sign_in', methods=['GET', 'POST'])
-def sign_in():
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        if user is not None and user.verify_password(form.password.data):
+        if user is not None and user.verify_password(
+                form.password.data):
             login_user(user)
             return redirect(url_for('home.dashboard'))
         else:
             flash('Invalid email or password.')
-    return render_template('auth/sign_in.html', form=form, title='Sign In')
-
+    return render_template('auth/login.html', form=form, title='Login')
 
 @auth.route('/logout')
 @login_required
@@ -45,4 +43,4 @@ def logout():
     flash('You have successfully been logged out.')
 
     # redirect to the login page
-    return redirect(url_for('auth.sign_in'))
+    return redirect(url_for('auth.login'))
