fbb9619e560e353d3e1befac5edd08e593bf2603
mnjaggah <maria.njaggah@gmail.com>
2017-02-16 17:43:27 +0300

    Import and register blueprint objects

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-#from flask_login import LoginManager
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from config import app_config
 from flask_login import LoginManager
@@ -25,6 +25,15 @@ def create_app(config_name):
 	migrate = Migrate(app, db)
 
 	from app import models
+
+	from .admin import admin as admin_blueprint
+    app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from .home import home as home_blueprint
+    app.register_blueprint(home_blueprint)
 
 	return app
 
