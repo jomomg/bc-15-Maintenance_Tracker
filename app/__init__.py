7c62506561bcc30d8a6d0a50ee86a56388da08ab
mnjaggah <maria.njaggah@gmail.com>
2017-02-17 10:38:02 +0300

    Fix bug relating to database

--- app/__init__.py
+++ app/__init__.py
@@ -5,39 +5,35 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
 from config import app_config #third party imports
+import os
 
-app = Flask(__name__)
-app.secret_key = '12345678'
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 #initializa databse variable
 db = SQLAlchemy()
 login_manager = LoginManager()
 
 def create_app(config_name):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'maintenancedb.sqlite')
+    app.secret_key = '12345678'
+    
+    db.init_app(app)
 
-	from .admin import admin as admin_blueprint
-	app = Flask(__name__, instance_relative_config=True)
-	app.config.from_object(app_config[config_name])
-	app.config.from_pyfile('config.py')
-	
-	db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_message = "You must be logged in to access this page."
+    login_manager.login_view ='auth.login'
 
-	login_manager.init_app(app)
-	login_manager.login_message = "You must be logged in to access this page."
-	login_manager.login_view ='auth.login'
+    
+    Bootstrap(app)
 
-	migrate = Migrate(app, db)
+    from app import models
 
+    from .admin import admin as admin_blueprint
+    app.register_blueprint(admin_blueprint, url_prefix='/admin')
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+    from .home import home as home_blueprint
+    app.register_blueprint(home_blueprint)
 
-	Bootstrap(app)
-
-	from app import models
-
-	from .admin import admin as admin_blueprint
-	app.register_blueprint(admin_blueprint, url_prefix='/admin')
-	from .auth import auth as auth_blueprint
-	app.register_blueprint(auth_blueprint)
-	from .home import home as home_blueprint
-	app.register_blueprint(home_blueprint)
-
-	return app
+    return app
