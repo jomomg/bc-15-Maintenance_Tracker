6e810d56c3768720a93e87eb7a805e0dca617433
mnjaggah <maria.njaggah@gmail.com>
2017-02-17 10:40:06 +0300

    Modify configuration environment

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
 import os
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from app import create_app, db
 
-from app import create_app
-
-
-config_name = os.getenv('FLASK_CONFIG')
+config_name = os.getenv('FLASK_CONFIG') or 'development'
 app = create_app(config_name)
 
 if __name__ == '__main__':
-    app.run()
+    migrate = Migrate(app, db)
+
+    manager = Manager(app)
+    manager.add_command('db', MigrateCommand)
+    manager.run()
